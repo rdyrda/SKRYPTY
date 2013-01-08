aea3da9a9973681b64bed6246edf9a455b2214d1
rdyrda <raxix_x@wp.pl>
2013-01-07 17:30:20 -0800

    Create Skrypt nr 4.sh

---
+++ Skrypt nr 4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ./skr9 plik rozmiar
+# Program dzieli plik na czesci po `rozmiar' wierszy.
+
+if [ $# -ne 2 ]; then
+    echo 'bledna liczba parametrow !'
+    exit
+fi
+
+rozmiar=`wc -l "$1"`
+rozmiar=${rozmiar% *}
+
+Nplk=0
+while [ $[ $2 * $Nplk ] -lt $rozmiar ]; do
+
+    cat "$1" | tail -n $[ $rozmiar - $2 * $Nplk ] | head -n $2 
+    echo '-------'
+
+    Nplk=$[ $Nplk + 1 ]
+
+done
